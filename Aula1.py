ff7383079a6307b6d7b3ef8c950c47953556c5cf
Gustavo Rodrigues <gumrodrigues07@gmail.com>
2026-05-01 05:30:27 -0300

    Projeto concluído

--- Aula1.py
+++ Aula1.py
@@ -14,35 +14,35 @@ print(faturamento)
 # quantidade de produtos vendidos por loja
 quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
 print(quantidade)
-print('-' * 50)
 
 # ticket médio produto vendido por loja
 ticket_medio = (faturamento["Valor Final"] / quantidade["Quantidade"]).to_frame()
+ticket_medio = ticket_medio.rename(columns={0: 'Ticket Médio'})
 print(ticket_medio)
 
 # enviar email com relatório
-mail.outlook = win32.Dispatch('outlook.application') # Créditos: Stack Flow
+mail.outlook = win32.Dispatch('outlook.application')
 mail = outlook.CreateItem(0)
-mail.To = 'nofanafarofa@gmail.com'
+mail.To = 'Seu email'
 mail.Subject = 'Relatório de Vendas'
-mail.HTMLBody = '''
-Prezados,
+mail.HTMLBody = f'''
+<p>Prezados,</p>
 
-segue o Relatório de Vendas por Loja.
+<p>segue o Relatório de Vendas por Loja.</p>
 
-Faturamento:
-{}
+<p>Faturamento:</p>
+{faturamento.to_html(formatters={'Valor Final': 'R${:,.2f}'.format})}
 
-Quantidade de Vendas:
-{}
+<p>Quantidade de Vendas:</p>
+{quantidade.to_html}
 
-Ticket Médio:
-{}
+<p>Ticket Médio:</p>
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R${:,.2f}'.format})}
 
-Qualquer dúvida estou à disposição
+<p>Qualquer dúvida estou à disposição</p>
 
-Att.,
-G. R.
+<p>Att.,</p>
+<p>G. R.</p>
 '''
 
 mail.Send()
